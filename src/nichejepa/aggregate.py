c42c83063cc677c2bd10953196f6fedea2e6d1fe
Adam Boxall <ab70@sanger.ac.uk>
2024-05-15 11:12:10 +0100

    add docs for aggregate_by_radius

--- src/nichejepa/aggregate.py
+++ src/nichejepa/aggregate.py
@@ -2,8 +2,23 @@ import anndata
 import squidpy as sq
 
 
-def aggregate_by_radius(adata: anndata.AnnData, radius=27.5) -> anndata.AnnData:
-    """Aggregate neighbourhood gene expression and """
+def aggregate_by_radius(adata: anndata.AnnData, radius: float = 27.5) -> anndata.AnnData:
+    """
+    Aggregate neighbourhood gene expression by radius
+
+    Parameters
+    ----------
+    adata : anndata.AnnData
+        AnnData object with spatial coordinates available in `adata.obsm["spatial"]`.
+    radius : float
+        Radius within which neighbouring cells will be aggregated, in um. Defaults to 27.5 um, which corresponds
+        to the 10x Visium spot size of 55 um.
+
+    Returns
+    ----------
+    adata : anndata.AnnData
+        AnnData object with aggregated counts available in `adata.layers["X_neighborhood"]`.
+    """
 
     sq.gr.spatial_neighbors(adata,
                             coord_type="generic",
