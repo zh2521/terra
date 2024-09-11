a387883d47af8a4c93d2e59e7a03daf3a9eb6921
Sebastian Birk <sb75@sanger.ac.uk>
2024-09-11 07:26:56 +0100

    Update docs in normalizers module

--- src/nichejepa/normalizers/normalize_by_cell_area.py
+++ src/nichejepa/normalizers/normalize_by_cell_area.py
@@ -3,25 +3,28 @@ import scipy.sparse as sp
 
 
 def normalize_by_cell_area(x: sp.csr_matrix,
-                           cell_areas: np.ndarray) -> sp.csr_matrix:
+                           cell_areas: np.ndarray
+                           ) -> sp.csr_matrix:
     """
     Normalize gene expression counts per cell by cell area.
 
     Parameters
     ----------
-    x: sp.csr_matrix
-        A sparse matrix where each row represents an observation and each column represents a feature.
-    cell_areas: np.ndarray
+    x:
+        A sparse matrix where each row represents an observation and each column
+        represents a feature.
+    cell_areas:
         Numpy array with the cell areas.
 
     Returns
     ----------
-    y: sp.csr_matrix
+    y:
         A sparse matrix containing the normalized features.
     """
 
     if x.shape[0] != len(cell_areas):
-        raise ValueError('Length of `cell_areas` does not match the number of observations in `x`.')
+        raise ValueError('Length of `cell_areas` does not match the number of'
+                         'observations in `x`.')
 
     y = x / cell_areas.reshape(-1, 1)
 
--- src/nichejepa/normalizers/normalize_by_read_depth.py
+++ src/nichejepa/normalizers/normalize_by_read_depth.py
@@ -1,20 +1,24 @@
 import scipy.sparse as sp
 
 
-def normalize_by_read_depth(x: sp.csr_matrix, target_size: int = 10_000) -> sp.csr_matrix:
+def normalize_by_read_depth(x: sp.csr_matrix,
+                            target_size: int=10_000
+                            ) -> sp.csr_matrix:
     """
     Normalize gene expression counts per cell by read depth.
 
     Parameters
     ----------
-    x: sp.csr_matrix
-        A sparse matrix where each row represents an observation and each column represents a feature.
-    target_size: int
-        The target read depth per observation (i.e. the sum of features across an observation).
+    x:
+        A sparse matrix where each row represents an observation and each column
+        represents a feature.
+    target_size:
+        The target read depth per observation (i.e. the sum of features across
+        an observation).
 
     Returns
     ----------
-    y: sp.csr_matrix
+    y:
         A sparse matrix containing the normalized features.
     """
 
