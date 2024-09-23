e5ff69d890174cc044b6bfa8a45bbaf6181a3dcd
Sebastian Birk <sb75@sanger.ac.uk>
2024-09-23 07:58:18 +0100

    Hotfix for token exclusion from agg

--- src/nichejepa/utils/embedding.py
+++ src/nichejepa/utils/embedding.py
@@ -150,12 +150,11 @@ def create_binary_selection_mask(tokens: torch.Tensor,
         selection_mask[:, (1 if has_cls else 0):
                           (1 if has_cls else 0) + seq_len_cell] = True
         selection_mask[tokens == 0] = False # exclude padding tokens
-        print(excluded_tokens)
         if excluded_tokens: # exclude other excluded tokens
-            print(selection_mask.sum())
             selection_mask[
-                torch.isin(tokens, torch.tensor(excluded_tokens))] = False
-            print(selection_mask.sum())
+                torch.isin(
+                    tokens,
+                    torch.tensor(excluded_tokens).to(tokens.device))] = False
         if top_k:
             # Exclude tokens beyond the top_k positions in the cell segment
             selection_mask[:, (1 if has_cls else 0) + top_k:] = False
@@ -166,7 +165,9 @@ def create_binary_selection_mask(tokens: torch.Tensor,
         selection_mask[tokens == 0] = False # exclude padding tokens
         if excluded_tokens: # exclude other excluded tokens
             selection_mask[
-                torch.isin(tokens, torch.tensor(excluded_tokens))] = False
+                torch.isin(
+                    tokens,
+                    torch.tensor(excluded_tokens).to(tokens.device))] = False
         if top_k:
             # Exclude tokens beyond the top_k positions in the neighborhood
             # segment
