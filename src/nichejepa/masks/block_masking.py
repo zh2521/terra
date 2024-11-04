Fix bug in block masking for cell graph tokenizer

src/nichejepa/masks/block_masking.py
@@ -103,9 +103,11 @@ class BlockMaskCollator:
             mask_ratio = self.per_block_mask_ratio
 
         # Get non-zero indices and segments excluding special tokens
-        nz_ns_indices = torch.nonzero(tokens).squeeze()[self.n_special_tokens:]
+        ns_tokens = tokens[self.n_special_tokens:]
+        nz_ns_indices = torch.nonzero(ns_tokens).squeeze()
         total_nz_ns = len(nz_ns_indices)
-        nz_ns_segments = segments[segments != 0][self.n_special_tokens:]
+        ns_segments = segments[self.n_special_tokens:]
+        nz_ns_segments = ns_segments[ns_segments != 0]
     
         # Initialize masks
         target_masks = []
@@ -129,7 +131,6 @@ class BlockMaskCollator:
 
             # Extract segments for the current block to determine which <cls>
             # are to be included
-            # 10 is index cell segment, corresponding to <cls> token at index 0
             block_segments = nz_ns_segments[start_idx: end_idx]
             block_unique_segments = torch.unique(block_segments)
             cls_tokens = [

src/nichejepa/train.py
@@ -381,8 +381,17 @@ def train(args: dict,
             masks_pred = [u.to(device, non_blocking=True) for u in masks_pred]
             masks_attention = masks_attention.to(device, non_blocking=True)
 
-            print(masks_enc)
-            print(masks_pred)
+            """
+            print(torch.count_nonzero(tokens[0]))
+            if masks_enc[0].shape[1] < 3:
+                print("CONTEXT")
+                print(masks_enc[0].shape)
+                print(masks_enc)
+            if masks_pred[0].shape[1] < 3:
+                print("TARGET")
+                print(masks_pred[0].shape)
+                print(masks_pred)
+            """ 
 
             # masks_pred = [masks_pred[0], masks_pred[-1]] # TEMP TODO
 