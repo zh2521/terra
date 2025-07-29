3a7d4a0dceab26b7ac874cfaa7c49400719f2623
Sebastian Birk <sb75@sanger.ac.uk>
2025-07-29 16:11:50 +0100

    Add more CPUs to AWS slurm script

--- src/app/train_model_slurm_torchrun_aws.sh
+++ src/app/train_model_slurm_torchrun_aws.sh
@@ -3,12 +3,12 @@
 #SBATCH --nodes=4                        # requests 4 nodes (each will typically run one process)
 #SBATCH --ntasks-per-node=1              # runs one task (process) per node and aligns with DDP across nodes
 #SBATCH --gpus-per-node=8                # requests 8 GPU on each node
-#SBATCH --cpus-per-task=1                # allocates 1 CPU cores per task
+#SBATCH --cpus-per-task=128              # allocates 128 CPU cores per task
 #SBATCH --mem=920G                       # allocates 920 GB of RAM memory per node
 #SBATCH --partition=p4de-24xlarge        # specifies the partition (queue) to submit the job to (use `sinfo` to see avaialble)
-#SBATCH --time=240:00:00                 # sets the max wall time (runtime) for the job (HH:MM:SS)
-#SBATCH --output=logs/aws/%j.out             # stdout file (%j is replaced with the job ID)
-#SBATCH --error=logs/aws/%j.err              # stder file (for logging errors)
+#SBATCH --time=480:00:00                 # sets the max wall time (runtime) for the job (HH:MM:SS)
+#SBATCH --output=logs/aws/%j.out         # stdout file (%j is replaced with the job ID)
+#SBATCH --error=logs/aws/%j.err          # stder file (for logging errors)
 #SBATCH --chdir=/home/ubuntu/sb75/nichejepa-reproducibility        # set working directory
 
 
@@ -41,7 +41,7 @@ export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 export MASTER_PORT=12345 
 
 export EXPERIMENT_NAME="hst_corpus_80m"
-export RUN_NAME="gtbase_aws_6"
+export RUN_NAME="gtbase_aws_7"
 
 echo "[+] SLURM_JOB_GPUS: $SLURM_JOB_GPUS"
 echo "[+] CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
