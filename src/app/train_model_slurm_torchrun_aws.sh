116fc389016604f4cb78801f549d2d36084958ac
Sebastian Birk <sb75@sanger.ac.uk>
2025-08-01 13:26:38 +0100

    Update AWS config

--- src/app/train_model_slurm_torchrun_aws.sh
+++ src/app/train_model_slurm_torchrun_aws.sh
@@ -41,7 +41,7 @@ export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 export MASTER_PORT=12345 
 
 export EXPERIMENT_NAME="hst_corpus_80m"
-export RUN_NAME="gtbase_aws_7"
+export RUN_NAME="gtbase_aws_8"
 
 echo "[+] SLURM_JOB_GPUS: $SLURM_JOB_GPUS"
 echo "[+] CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
