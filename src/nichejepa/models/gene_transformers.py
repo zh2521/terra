6370aadfdb8d2671096e15e74547d952a256b410
sebastianbirk <sebastian.birk@outlook.com>
2026-02-22 10:25:30 +0000

    Add zeroing out of non inluded cells during inference

--- src/nichejepa/models/gene_transformers.py
+++ src/nichejepa/models/gene_transformers.py
@@ -226,8 +226,8 @@ class GeneTransformerBaseEncoder(ABC, nn.Module):
                     self.n_special_tokens:(self.n_special_tokens+self.seq_len_cell),
                     (self.n_special_tokens+self.seq_len_cell):] = False
 
-        #if cell_only:
-        #    x[:, (self.n_special_tokens+self.seq_len_cell):, :] = 0
+        if cell_only:
+            x[:, (self.n_special_tokens+self.seq_len_cell):, :] = 0
 
             #torch.set_printoptions(profile="full")
             #print(attn.shape)
